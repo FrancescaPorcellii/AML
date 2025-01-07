e7c4f3b76a7a2cdb015f24e82dadcf82f3540028
Claudia Maggiulli <s332252@studenti.polito.it>
2025-01-07 15:16:56 +0100

    nsecondo punto nuova option

--- episodic-memory/NLQ/VSLNet/options.py
+++ episodic-memory/NLQ/VSLNet/options.py
@@ -203,5 +203,14 @@ def read_command_line():
         default=None,
         help="A list of splits to remove empty queries from. Valid values for the list are: ['train', 'val']",  # noqa
     )
+
+    parser.add_argument(
+        "--encoder_shared",
+        type=bool,
+        default=True,
+        help="Whether to share the encoder between video and text",
+    )
+
     configs = parser.parse_args()
+
     return configs, parser
